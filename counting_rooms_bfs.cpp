64005bcb3be31c51bd73df9908e4db9bafa95959
Ashutosh Ranjan <ashutoshranjan@kgpian.iitkgp.ac.in>
2026-05-27 16:18:34 +0530

    Intial commit

--- counting_rooms_bfs.cpp
+++ counting_rooms_bfs.cpp
@@ -1,47 +1,58 @@
 /***********************************************
  Author        : Akiramenai_
- Created On    : 27/05/2026 14:49:11
+ Created On    : 27/05/2026 15:19:29
 ************************************************/
 #include <bits/stdc++.h>
 using namespace std;
 
-//solved using dfs
+//solving using bfs
 #define int long long
 #define F first
 #define S second
-using state =pair<int,int>;
+using state=pair<int,int>;
+const int INF=1e9;
 
 int dx[]={0,1,-1,0};
 int dy[]={1,0,0,-1};
 
-//find number of components
 int n,m;
 vector<string>arr;
 
 vector<vector<int>>visited;
+
 bool is_valid(int x,int y){
     if(x>=0 && x<n && y>=0 && y<m && arr[x][y]=='.')return true;
     else return false;
 }
 
-void dfs(state node){
-    visited[node.F][node.S]=1;
+void bfs(state st){
 
-    for(int i=0;i<4;i++){
-        int nx=node.F+dx[i];
-        int ny=node.S+dy[i];
+    queue<state>q;
+    q.push(st);
 
-        if(is_valid(nx,ny)&&(!visited[nx][ny])){
-            dfs({nx,ny});
+    while(!q.empty()){
+        state cur=q.front();
+        q.pop();
+
+        if(visited[cur.F][cur.S])continue;
+        visited[cur.F][cur.S]=1;
+
+        for(int i=0;i<4;i++){
+            int nx=cur.F+dx[i];
+            int ny=cur.S+dy[i];
+
+            if(is_valid(nx,ny)&&(!visited[nx][ny])){
+                q.push({nx,ny});
+            }
         }
     }
 }
+
 void solve()
 {
     cin>>n>>m;
     arr.resize(n);
     visited.assign(n,vector<int>(m,0));
-
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
@@ -50,7 +61,7 @@ void solve()
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             if(arr[i][j]=='.'&&(!visited[i][j])){
-                dfs({i,j});
+                bfs({i,j});
                 cnt++;
             }
         }
